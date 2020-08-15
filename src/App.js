a41bb14f0ff3e732541daeaca0c13eb871e3b8d4
Sercantugba <sercantugba834@gmail.com>
2020-08-15 14:16:03 -0500

    add gender

--- src/App.js
+++ src/App.js
@@ -48,6 +48,12 @@ export default class App extends Component {
         </label>
       </form>
       <form onSubmit={this.handleChange}>
+      {/* <label>  
+          Gender: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}> */}
       <label>  
           Breed: 
           <input type="text"  value={this.state.value} onChange={this.handleChange}/>
